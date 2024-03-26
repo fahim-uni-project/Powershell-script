71d18f1f759eca6baed60e3282f793eb9e5fd0b3
fahim-uni-project <48731538+fahim-uni-project@users.noreply.github.com>
2024-03-26 13:23:43 +0000

    Add files via upload

---
+++ getproductkey.ps1
@@ -0,0 +1,5 @@
+﻿# Get the Windows product key
+$windowsProductKey = (Get-WmiObject -query 'select * from SoftwareLicensingService').OA3xOriginalProductKey
+
+# Display the product key
+Write-Host "Windows Product Key: $windowsProductKey"
