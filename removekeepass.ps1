141a23389ca25b6fc6514841f987732df8a96809
fahim-uni-project <48731538+fahim-uni-project@users.noreply.github.com>
2024-03-26 13:17:07 +0000

    Uninstall Keeypass

---
+++ removekeepass.ps1
@@ -0,0 +1,7 @@
+﻿$paths = @("C:\Program Files\KeePass Password Safe 2\unins000.exe", "C:\Program Files (x86)\KeePass Password Safe 2\unins000.exe")
+$uninstall = $paths | Where-Object { Test-Path $_ } | Select-Object -First 1
+if ($uninstall) {
+    Start-Process -FilePath $uninstall -ArgumentList "/VERYSILENT /NORESTART" -Wait -NoNewWindow
+} else {
+    Write-Host "The file does not exist in either path."
+}
