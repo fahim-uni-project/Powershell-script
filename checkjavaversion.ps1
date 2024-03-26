41e4bae7ff4eb21d0d571a6c1a4f9a9d220383c4
fahim-uni-project <48731538+fahim-uni-project@users.noreply.github.com>
2024-03-26 13:11:21 +0000

    Powershell to check Java version

---
+++ checkjavaversion.ps1
@@ -0,0 +1,13 @@
+﻿# Get the registry key for Chrome
+$registryPath32 = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
+
+$installedApps = @()
+
+# Query 32-bit registry path
+Get-ChildItem -Path $registryPath32 | Where-Object { $_.GetValue("DisplayName") -like "Java 8 Update 401*" } | ForEach-Object {
+    $app = New-Object -TypeName PSObject
+    $app | Add-Member -MemberType NoteProperty -Name "DisplayName" -Value $_.GetValue("DisplayName")
+    $app | Add-Member -MemberType NoteProperty -Name "Version" -Value $_.GetValue("DisplayVersion")
+    $installedApps += $app
+}
+$installedApps 
